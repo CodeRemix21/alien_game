045f3d032bf7945f898aa2524679dafed401c208
CodeRemix21 <trytekremik@gmail.com>
2024-10-22 17:17:59 +0200

    dodanie pociskow, refaktoryzacja

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Klasa ogólna przeznaczona do zarządzania zasobami i sposobem dzialania gry"""
@@ -19,12 +20,14 @@ class AlienInvasion:
 
         # Tworzenie instancji statku
         self.ship = Ship(self)
+        # Tworzenie instancji pocisków
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Rozpoczęcie gry"""
         while True:
             self._check_events()
-            self._update_sreen()
+            self._update_screen()
             self.clock.tick(self.settings.fps)
 
     def _check_events(self):
@@ -38,13 +41,15 @@ class AlienInvasion:
                 self._check_keyup_event(event)
     
     def _check_keydown_event(self, event: pygame.event):
-        """Reakcja na uopuszczenie klawisza"""
+        """Reakcja na upuszczenie klawisza"""
         if event.key == pygame.K_q:
             sys.exit()
         elif event.key == pygame.K_RIGHT:
             self.ship.move_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.move_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_event(self, event: pygame.event):
         """Reakcja na naciśnięcie klawisza"""
@@ -53,13 +58,33 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.move_left = False
 
-    def _update_sreen(self):
+    def _update_screen(self):
         """Uaktualnienie obrazów na ekranie"""
-        self.ship.update_position()
         self.screen.fill(self.settings.bg_color)
-        self.ship.blitme()
+        self._update_ship()
+        self._update_bullets()
+
         pygame.display.flip()
 
+    def _fire_bullet(self):
+        """Utworzenie nowego pocisku i dodanie do grupy"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_ship(self):
+        """Uaktualnienie statku na ekranie"""
+        self.ship.update_position()
+        self.ship.blitme()
+    
+    def _update_bullets(self):
+        """Uaktualnienie pocisków na ekranie"""
+        for bullet in self.bullets.sprites():
+            bullet.update_position()
+            bullet.blitme()
+            # Jeżeli pocisk poza ekranem -> usuń
+            if bullet.rect.y <= 0:
+                self.bullets.remove(bullet)
 
 if __name__ == "__main__":
     ai = AlienInvasion()
